4f55c3302013a680eb72969f43cadbc404c93c0b
liangjiajun <liangjiajun@cvte.com>
2016-11-15 21:43:23 +0800

    modified template.cpp and rm buff.txt

--- template.cpp
+++ template.cpp
@@ -1,34 +1,18 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-template<typename arrayType>
-class MyArray{
-public:
-    MyArray(arrayType &a,arrayType &b){
-        array1 = a;
-        array2 = b;
-    }
-    
-    arrayType getArrayNum(arrayType array,int num){
-        if(num < 0 || num > 10){
-            cout << "out of index" << endl;
-        }
-
-        return array[num];
-    }
-
-private:
-    arrayType array1[10];
-    arrayType array2[10]; 
-
-};
-
 
 int main(){
-    int* intArray = new int[10];
-    for(int i = 0 ; i < 10 ;i++){
-        intArray[i] = i;
-    } 
+    long i = 100000000l;
+    clock_t start,finish;
+    cout << "Time :" ;
+    start = clock();
+    while(i--);
+    finish = clock();
+    double duration = (double)(finish - start);/// CLOCKS_PER_SEC;
+    cout << duration << " ms" << endl;
+ 
     return 1;
 }
